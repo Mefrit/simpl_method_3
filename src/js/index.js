6eca94bac45904a34b5ff1dd95e63905a6b080d7
Михаил Фокин <mihail_fokin_1999@mail.ru>
2020-11-07 15:28:44 +0300

    add line

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,11 @@
-window.onload = function () {
+window.onload = function() {
     let start_config = {
         a: 1,
         b: 1,
         g: 1,
         d: 1,
-        start_segment: -3,
-        end_segment: 4,
+        start_segment: -2,
+        end_segment: 2,
         min: -2,
         max: 2,
         upper_limit: 2,
@@ -15,7 +15,9 @@ window.onload = function () {
         accuracy_rynge: 0.001,
         curent_parameter: "g",
     };
-    let obj = new NumericalMetods(document.getElementById("canvas"), start_config);
+    let canvas = document.getElementById("canvas");
+    let obj = new NumericalMetods(canvas, start_config);
+    canvas.onclick = obj.setPosition;
 
     function startMetod(event) {
         obj.start();
@@ -62,11 +64,44 @@ class NumericalMetods {
         this.clear = false;
         this.accuracy_rynge = config.accuracy_rynge;
         this.ready = false;
+        this.start_x = 1;
+        this.start_y = 2;
+
+    }
+    setPosition = (e) => {
+        console.log(event.screenX, "-", this.canvas.width, "|", event.screenY, "-", this.canvas.height);
+        let x, y;
+        let bbox = this.canvas.getBoundingClientRect()
+        x = e.clientX - bbox.left * (this.canvas.width / bbox.width);
+        y = e.clientY - bbox.top * (this.canvas.height / bbox.height);
+        this.start_x = (x - bbox.width / 2) + 20;
+        this.start_y = (y - bbox.height / 2);
+        // if (this.canvas.height / 2 > event.screenY) {
+        //     this.start_y = this.canvas.height - event.screenY;
+        // } else {
+        //     this.start_y = event.screenY - this.canvas.height / 2;
+        // }
+
+        console.log("this.start_x, this.start_y =>", this.start_x, this.start_y, "\n\n", "height", this.canvas.height / 2, "screenY", event.screenY);
+        this.startСomputing().then(() => {
+            document.getElementById("status").style.display = "none";
+        });
+        // if(event.screenX > this.canvas.width){
+        //     this.start_x = event.screenX - 
+        // } else{
+
+        // }
+        // if(event.screenX > this.canvas.width){
+
+        // } else{
+
+        // }
+
     }
     getFunction(x, y) {
         let result = { x: 0, y: 0 };
         result.x = 1 * x + 1 * y + 1 * y * x + 1;
-        result.y = 1 * x + 1 * y - 1 * y * y * y + 33;
+        result.y = 1 * x + 1 * y + 1 * y * y * y + 1;
         // result.x = 1 * x + 1 * y;
         // result.y = (1 * x - 1) * (y + x);
         // if (this.g != x) {
@@ -149,18 +184,39 @@ class NumericalMetods {
         }
         return (this.step_integral / n) * res;
     }
-    drawResult(result_intergral, i) {
+    drawResult(list, color = "black") {
         let scaleX = this.canvas.width / (this.end_segment - this.start_segment),
             scaleY = this.canvas.height / (this.max - this.min);
+        this.ctx.lineWidth = 0.6;
         this.ctx.beginPath();
-        this.ctx.fillRect(
-            Math.round(-this.start_segment * scaleX + i * scaleX),
-            Math.round(this.max * scaleY - result_intergral * scaleY),
-            this.scale_parametrs == 1 ? 4 : 1,
-            this.scale_parametrs == 1 ? 4 : 1
-        );
+        this.ctx.strokeStyle = color;
+        this.ctx.moveTo(this.canvas.width / 2 + this.start_x, this.canvas.height / 2 + this.start_y);
+        list.forEach(elem => {
+            // console.log(elem);
+            this.ctx.lineTo(elem.x * scaleX, elem.y * scaleY);
+            // this.ctx.stroke();
+        });
+
         this.ctx.stroke();
         this.ctx.closePath();
+
+        //     this.ctx.beginPath();
+        //     this.ctx.fillRect(
+        //         Math.round(-this.start_segment * scaleX + i * scaleX),
+        //         Math.round(this.max * scaleY - result_intergral * scaleY),
+        //         this.scale_parametrs == 1 ? 4 : 1,
+        //         this.scale_parametrs == 1 ? 4 : 1
+        //     );
+        //     this.ctx.stroke();
+        //     this.ctx.closePath();
+    }
+    checkParametr(par) {
+        if (par < 0) {
+            return par > -10000 ? par : -10000;
+        } else {
+            return par > 10000 ? 10000 : par;
+        }
+
     }
     startСomputing(n) {
         return new Promise((resolve) => {
@@ -172,26 +228,43 @@ class NumericalMetods {
                 }
             };
             checkLoad();
-            let curent_x = 1,
-                curent_y = 2, // начальные условия системы ( куда тыкаем н а канвасе )
-                n = 20,
+            let curent_x = this.start_x,
+                curent_y = this.start_y, // начальные условия системы ( куда тыкаем н а канвасе )
+                n = 5,
                 res,
                 list = [],
                 x,
                 y;
-            this.scale_parametrs = 1; // шаг перехода
+            this.scale_parametrs = 0.1; // шаг перехода
+            for (let i = 0; i < n; i++) {
+                res = this.getFunction(curent_x, curent_y);
+                curent_x = curent_x - this.scale_parametrs * res.x;
+                curent_y = curent_y - this.scale_parametrs * res.y;
+
+                curent_x = this.checkParametr(curent_x);
+                curent_y = this.checkParametr(curent_y);
+                list.push({ x: curent_x, y: curent_y });
+            }
+            this.drawResult(list, "green");
+            curent_x = this.start_x;
+            curent_y = this.start_y;
+            console.log("list 1", list);
+            list = [];
+
             for (let i = 0; i < n; i++) {
                 res = this.getFunction(curent_x, curent_y);
                 curent_x = curent_x + this.scale_parametrs * res.x;
                 curent_y = curent_y + this.scale_parametrs * res.y;
+
+                curent_x = this.checkParametr(curent_x);
+                curent_y = this.checkParametr(curent_y);
                 list.push({ x: curent_x, y: curent_y });
-                // y = curent_x + this.scale_parametrs * res.x;
-                // x = curent_y + this.scale_parametrs * res.y;
-                // list.push({ x: x, y: y });
-                // x = curent_x;
-                // y = curent_y;
             }
-            console.log("list", list);
+
+
+
+            console.log("list 2", list);
+            this.drawResult(list, "red");
             // let result_1 = 0,
             //     result_2 = 2,
             //     coef_step = 1,
